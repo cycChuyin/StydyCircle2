58069217a4c8edcc2713739dd15f6e6f8ef9eb8c
chuyin <draw136also761@gmail.com>
2021-11-08 19:58:35 +0800

    活動三個主題頁面製作

--- src/views/Activity/ThreeThemeActivity.vue
+++ src/views/Activity/ThreeThemeActivity.vue
@@ -1,7 +1,123 @@
 <template>
-  <div>三個主題活動頁</div>
-  <router-link :to="`/activity/online`">online</router-link>｜
-  <router-link :to="`/activity/entity`">entity</router-link>｜
-  <router-link :to="`/activity/workshop`">workshop</router-link>
+  <!-- navbar 導覽列 -->
+  <component-navbar></component-navbar>
+  <!-- banner -->
+  <div class="banner bg-searchselect">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-5 py-9 pt-10">
+          <h6 class="text-secondary display-6 fw-bold mb-3">線上讀書會</h6>
+          <p class="text-secondary fs-4">
+            線上讀書會的宗旨是希望閱讀、分享不再局限於地點的距離，不論您是在地球的另一邊，我們都很歡迎大家一起來參加！
+          </p>
+        </div>
+      </div>
+      <!-- 頁籤 -->
+      <div class="row justify-content-start">
+        <div class="col-md-6">
+          <!-- 頁籤 -->
+          <ul
+            class="
+              nav nav-tabs
+              d-flex
+              justify-content-start
+              list-unstyled
+              border-0
+            "
+          >
+            <li
+              class="nav-item rounded-4 rounded-bottom-0"
+              :class="{ linkActive: isOnline }"
+              aria-current="true"
+              aria-selected="true"
+              @click="changeActive('online')"
+            >
+              <router-link
+                class="nav-link px-32 py-3 rounded-4 rounded-bottom-0"
+                type="button"
+                :to="`/activity/online`"
+                >線上讀書會</router-link
+              >
+            </li>
+            <li
+              class="nav-item rounded-4 rounded-bottom-0"
+              :class="{ linkActive: isEntity }"
+              aria-selected="false"
+              @click="changeActive('entity')"
+            >
+              <router-link
+                class="nav-link px-32 py-3 rounded-4 rounded-bottom-0"
+                type="button"
+                :to="`/activity/entity`"
+                >實體讀書會</router-link
+              >
+            </li>
+            <li
+              class="nav-item rounded-4 rounded-bottom-0"
+              :class="{ linkActive: isWorkShop }"
+              aria-selected="false"
+              @click="changeActive('workshop')"
+            >
+              <router-link
+                class="nav-link px-32 py-3 rounded-4 rounded-bottom-0"
+                type="button"
+                :to="`/activity/workshop`"
+                >活動工作坊</router-link
+              >
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+
   <router-view></router-view>
+  <!-- 下方 footer -->
+  <component-footer></component-footer>
 </template>
+
+<style>
+.linkActive {
+  color: #495057;
+  background-color: #fff;
+  border-color: #dee2e6 #dee2e6 #fff;
+  border-bottom: 0;
+  font-weight: bold;
+}
+</style>
+
+<script>
+import componentNavbar from '@/components/Layout/Navbar.vue'
+import componentFooter from '@/components/Layout/Footer.vue'
+
+export default {
+  data () {
+    return {
+      isOnline: true,
+      isEntity: false,
+      isWorkShop: false
+    }
+  },
+  components: {
+    componentNavbar,
+    componentFooter
+  },
+  methods: {
+    changeActive (type) {
+      if (type === 'online') {
+        this.isOnline = true
+        this.isEntity = false
+        this.isWorkShop = false
+      } else if (type === 'entity') {
+        this.isOnline = false
+        this.isEntity = true
+        this.isWorkShop = false
+      } else if (type === 'workshop') {
+        this.isOnline = false
+        this.isEntity = false
+        this.isWorkShop = true
+      }
+    }
+  }
+}
+</script>
--- src/views/Activity/WorkShop.vue
+++ src/views/Activity/WorkShop.vue
@@ -1,3 +1,378 @@
 <template>
-    我是工作坊頁面
+  <!-- 內容 -->
+  <div class="bg-white">
+    <div class="container">
+      <!-- 篩選搜尋區塊 -->
+      <div class="row py-5">
+        <div class="col-md-10">
+          <div
+            class="
+              rounded-pill
+              border border-secondary
+              d-flex
+              align-items-center
+            "
+          >
+            <div class="input-group px-4 py-13">
+              <button
+                class="
+                  btn btn-outline-secondary
+                  dropdown-toggle
+                  border-0
+                  text-dark
+                  fw-light
+                  border-end border-dark
+                  bg-transparent
+                  py-0
+                "
+                type="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                類別
+              </button>
+              <ul
+                class="dropdown-menu text-dark fw-light p-0 border-0 rounded-3"
+                aria-labelledby="defaultDropdown"
+              >
+                <li>
+                  <a class="dropdown-item py-2 text-center rounded-3" href="#"
+                    >所有分類</a
+                  >
+                </li>
+                <li>
+                  <a class="dropdown-item py-2 text-center rounded-3" href="#"
+                    >商業理財</a
+                  >
+                </li>
+                <li>
+                  <a class="dropdown-item py-2 text-center rounded-3" href="#"
+                    >文學小說</a
+                  >
+                </li>
+                <li>
+                  <a class="dropdown-item py-2 text-center rounded-3" href="#"
+                    >人文史地</a
+                  >
+                </li>
+                <li>
+                  <a class="dropdown-item py-2 text-center rounded-3" href="#"
+                    >醫療保健</a
+                  >
+                </li>
+                <li>
+                  <a class="dropdown-item py-2 text-center rounded-3" href="#"
+                    >生活風格</a
+                  >
+                </li>
+                <li>
+                  <a class="dropdown-item py-2 text-center rounded-3" href="#"
+                    >自然科學</a
+                  >
+                </li>
+                <li>
+                  <a class="dropdown-item py-2 text-center rounded-3" href="#"
+                    >電腦資訊</a
+                  >
+                </li>
+              </ul>
+              <button
+                class="
+                  btn btn-outline-secondary
+                  dropdown-toggle
+                  border-0
+                  text-dark
+                  fw-light
+                  border-end border-dark
+                  fs-6
+                  bg-transparent
+                  py-0
+                "
+                type="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                地點
+              </button>
+              <ul
+                class="dropdown-menu text-dark fw-light p-0 border-0 rounded-3"
+                aria-labelledby="defaultDropdown"
+              >
+                <li>
+                  <a class="dropdown-item py-2 text-center rounded-3" href="#"
+                    >基隆市</a
+                  >
+                </li>
+                <li>
+                  <a class="dropdown-item py-2 text-center rounded-3" href="#"
+                    >台北市</a
+                  >
+                </li>
+              </ul>
+              <input
+                type="text"
+                class="
+                  form-control form-control-darkGray
+                  border-0
+                  position-relative
+                  ps-3
+                  bg-transparent
+                "
+                aria-label="Text input with dropdown button"
+                placeholder="請輸入關鍵字搜尋（書籍名稱、活動名稱等）"
+              />
+            </div>
+          </div>
+        </div>
+        <div class="col-md-2">
+          <button
+            class="
+              btn
+              bg-secondary
+              fs-6
+              py-13
+              rounded-pill
+              text-white
+              w-100
+              h-100
+              d-flex
+              justify-content-center
+              align-items-center
+            "
+            type="button"
+          >
+            <span class="material-icons me-3">search</span>
+            搜尋
+          </button>
+        </div>
+      </div>
+      <!-- 即將截止 -->
+      <div class="mb-5">
+        <!-- 標題 -->
+        <div class="d-flex justify-content-between align-items-center mb-4">
+          <h6 class="display-6 text-secondary fw-bold">
+            即將截止報名的活動工作坊
+          </h6>
+          <a href="#" class="text-secondary fs-4 d-flex align-items-center">
+            <span class="material-icons me-2">east</span>
+            More
+          </a>
+        </div>
+        <!-- 卡片區塊 -->
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+          <div class="col">
+            <div class="card h-100 rounded-5">
+              <img
+                src="style/photo/ludemeula-fernandes-9UUoGaaHtNE-unsplash.jpg"
+                class="card-img-top card-img"
+                alt="..."
+              />
+              <div class="card-img-overlay d-flex justify-content-between">
+                <span class="material-icons text-white fs-1"
+                  >bookmark_border</span
+                >
+              </div>
+              <div class="card-body p-4">
+                <h5 class="card-title mb-2 p-0 fs-4">
+                  <a href="#" class="stretched-link text-secondary"
+                    >《新手貓奴必看》</a
+                  >
+                </h5>
+                <div class="d-flex align-items-center mb-2">
+                  <div class="d-flex align-items-center">
+                    <span class="material-icons text-primary me-1"
+                      >star_rate</span
+                    >
+                    <span class="material-icons text-primary me-1"
+                      >star_rate</span
+                    >
+                    <span class="material-icons text-primary me-1"
+                      >star_rate</span
+                    >
+                    <span class="material-icons text-primary me-1"
+                      >star_rate</span
+                    >
+                    <span class="material-icons text-primary">star_rate</span>
+                  </div>
+                  <p class="text-gray m-0 ps-2">5/5 (25則評論)</p>
+                </div>
+                <p class="text-secondary mb-4">
+                  <span class="pe-13">2021 / 10 / 12</span>｜<span class="px-13"
+                    >13:00 - 15:00</span
+                  >｜<span class="ps-13">王小明</span>
+                </p>
+                <div
+                  class="
+                    card-footer
+                    border-0
+                    bg-transparent
+                    p-0
+                    d-flex
+                    justify-content-between
+                    align-items-end
+                  "
+                >
+                  <p class="text-secondary fs-8 m-0">10人參加 ｜ 25人收藏</p>
+                  <p class="text-secondary fs-4 m-0">NT$ 100</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- 最多人報名 -->
+      <div class="mb-5">
+        <!-- 標題 -->
+        <div class="d-flex justify-content-between align-items-center mb-4">
+          <h6 class="display-6 text-secondary fw-bold">
+            最多人報名的活動工作坊
+          </h6>
+          <a href="#" class="text-secondary fs-4 d-flex align-items-center">
+            <span class="material-icons me-2">east</span>
+            More
+          </a>
+        </div>
+        <!-- 卡片區塊 -->
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+          <div class="card h-100 rounded-5">
+            <img
+              src="style/photo/ludemeula-fernandes-9UUoGaaHtNE-unsplash.jpg"
+              class="card-img-top card-img"
+              alt="..."
+            />
+            <div class="card-img-overlay d-flex justify-content-between">
+              <span class="material-icons text-white fs-1"
+                >bookmark_border</span
+              >
+            </div>
+            <div class="card-body p-4">
+              <h5 class="card-title mb-2 p-0 fs-4">
+                <a href="#" class="stretched-link text-secondary"
+                  >《新手貓奴必看》</a
+                >
+              </h5>
+              <div class="d-flex align-items-center mb-2">
+                <div class="d-flex align-items-center">
+                  <span class="material-icons text-primary me-1"
+                    >star_rate</span
+                  >
+                  <span class="material-icons text-primary me-1"
+                    >star_rate</span
+                  >
+                  <span class="material-icons text-primary me-1"
+                    >star_rate</span
+                  >
+                  <span class="material-icons text-primary me-1"
+                    >star_rate</span
+                  >
+                  <span class="material-icons text-primary">star_rate</span>
+                </div>
+                <p class="text-gray m-0 ps-2">5/5 (25則評論)</p>
+              </div>
+              <p class="text-secondary mb-4">
+                <span class="pe-13">2021 / 10 / 12</span>｜<span class="px-13"
+                  >13:00 - 15:00</span
+                >｜<span class="ps-13">王小明</span>
+              </p>
+              <div
+                class="
+                  card-footer
+                  border-0
+                  bg-transparent
+                  p-0
+                  d-flex
+                  justify-content-between
+                  align-items-end
+                "
+              >
+                <p class="text-secondary fs-8 m-0">10人參加 ｜ 25人收藏</p>
+                <p class="text-secondary fs-4 m-0">NT$ 100</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- 本週新推出 -->
+      <div class="mb-5">
+        <!-- 標題 -->
+        <div class="d-flex justify-content-between align-items-center mb-4">
+          <h6 class="display-6 text-secondary fw-bold">
+            本週新推出的活動工作坊
+          </h6>
+          <a href="#" class="text-secondary fs-4 d-flex align-items-center">
+            <span class="material-icons me-2">east</span>
+            More
+          </a>
+        </div>
+        <!-- 卡片區塊 -->
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+          <div class="card h-100 rounded-5">
+            <img
+              src="style/photo/ludemeula-fernandes-9UUoGaaHtNE-unsplash.jpg"
+              class="card-img-top card-img"
+              alt="..."
+            />
+            <div class="card-img-overlay d-flex justify-content-between">
+              <span class="material-icons text-white fs-1"
+                >bookmark_border</span
+              >
+            </div>
+            <div class="card-body p-4">
+              <h5 class="card-title mb-2 p-0 fs-4">
+                <a href="#" class="stretched-link text-secondary"
+                  >《新手貓奴必看》</a
+                >
+              </h5>
+              <div class="d-flex align-items-center mb-2">
+                <div class="d-flex align-items-center">
+                  <span class="material-icons text-primary me-1"
+                    >star_rate</span
+                  >
+                  <span class="material-icons text-primary me-1"
+                    >star_rate</span
+                  >
+                  <span class="material-icons text-primary me-1"
+                    >star_rate</span
+                  >
+                  <span class="material-icons text-primary me-1"
+                    >star_rate</span
+                  >
+                  <span class="material-icons text-primary">star_rate</span>
+                </div>
+                <p class="text-gray m-0 ps-2">5/5 (25則評論)</p>
+              </div>
+              <p class="text-secondary mb-4">
+                <span class="pe-13">2021 / 10 / 12</span>｜<span class="px-13"
+                  >13:00 - 15:00</span
+                >｜<span class="ps-13">王小明</span>
+              </p>
+              <div
+                class="
+                  card-footer
+                  border-0
+                  bg-transparent
+                  p-0
+                  d-flex
+                  justify-content-between
+                  align-items-end
+                "
+              >
+                <p class="text-secondary fs-8 m-0">10人參加 ｜ 25人收藏</p>
+                <p class="text-secondary fs-4 m-0">NT$ 100</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div class="d-flex justify-content-center">
+        <button
+          type="button"
+          class="btn btn-secondary rounded-pill mt-5 mb-10 text-white"
+        >
+          查看所有結果
+        </button>
+      </div>
+    </div>
+  </div>
 </template>
